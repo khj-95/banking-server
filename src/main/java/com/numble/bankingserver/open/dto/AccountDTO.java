6fe2354cc1aa1c1940b9893f4ba78d5ea20dfa22
khj <kim37171@cuk.edu>
2023-02-26 05:01:12 +0900

    refactor: AccountDTO

--- src/main/java/com/numble/bankingserver/open/dto/AccountDTO.java
+++ src/main/java/com/numble/bankingserver/open/dto/AccountDTO.java
@@ -2,18 +2,23 @@ package com.numble.bankingserver.open.dto;
 
 import com.numble.bankingserver.open.vo.OpenAccountVO;
 import com.numble.bankingserver.user.domain.User;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-@Builder
 public class AccountDTO {
 
     private User user;
     private String accountNumber;
     private long balance;
+
+    @Builder
+    private AccountDTO(User user, String accountNumber, long balance) {
+        this.user = user;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
 
     public static AccountDTO convertToAccountDTO(OpenAccountVO vo, User user) {
         return AccountDTO.builder()
@@ -21,5 +26,25 @@ public class AccountDTO {
             .accountNumber(vo.getAccountNumber())
             .balance(vo.getBalance())
             .build();
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof AccountDTO) {
+            AccountDTO tmp = (AccountDTO) obj;
+            return user.equals(tmp.getUser())
+                && accountNumber.equals(tmp.getAccountNumber())
+                && balance == tmp.getBalance();
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, accountNumber, balance);
     }
 }
