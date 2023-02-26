a000bb23bd6d05effc3e888dfd4eb9d25308cb46
khj <kim37171@cuk.edu>
2023-02-26 09:23:31 +0900

    refactor: FollowVO

--- src/main/java/com/numble/bankingserver/follow/vo/FollowVO.java
+++ src/main/java/com/numble/bankingserver/follow/vo/FollowVO.java
@@ -4,20 +4,36 @@ import com.numble.bankingserver.follow.domain.Follow;
 import com.numble.bankingserver.user.domain.User;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 @Getter
-@AllArgsConstructor
-@Builder
 public class FollowVO {
 
     private String fromUserId;
     private String fromUserName;
     private String toUserId;
     private String toUserName;
+
+    @Builder
+    private FollowVO(String fromUserId, String fromUserName, String toUserId, String toUserName) {
+        this.fromUserId = fromUserId;
+        this.fromUserName = fromUserName;
+        this.toUserId = toUserId;
+        this.toUserName = toUserName;
+    }
+
+    public static FollowVO createFollowVO(
+        String fromUserId, String fromUserName, String toUserId, String toUserName) {
+        return FollowVO.builder()
+            .fromUserId(fromUserId)
+            .fromUserName(fromUserName)
+            .toUserId(toUserId)
+            .toUserName(toUserName)
+            .build();
+    }
 
     public static List<FollowVO> toFollowList(Page<Follow> followList) {
         List<FollowVO> followVOList = new ArrayList<>();
@@ -34,5 +50,22 @@ public class FollowVO {
             followVOList.add(vo);
         }
         return followVOList;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof FollowVO) {
+            FollowVO tmp = (FollowVO) obj;
+            return fromUserId.equals(tmp.getFromUserId())
+                && fromUserName.equals(tmp.getFromUserName())
+                && toUserId.equals(tmp.getToUserId())
+                && toUserName.equals(tmp.getToUserName());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fromUserId, fromUserName, toUserId, toUserName);
     }
 }
